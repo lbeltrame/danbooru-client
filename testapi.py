e5b414be27a886da7e7c41389e0b5f129409a2dc
Luca Beltrame <einar@heavensinferno.net>
2009-09-26 18:34:15 +0200

    Proper unit tests for the functions implemented so far.

--- testapi.py
+++ testapi.py
@@ -16,33 +16,73 @@
 #   Free Software Foundation, Inc.,
 #   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import sys
 import unittest
 
-from PyQt4.QtCore import QString
-from PyQt4.QtGui import QImage
-from PyKDE4.kdecore import KUrl, KTemporaryFile
+from PyKDE4.kdeui import *
+from PyKDE4.kdecore import *
 from PyKDE4.kio import KIO
 
 import api
 
 "Module that provides unit tests for the Danbooru API"
 
-class testAPI(unittest.TestCase):
+app_name="DanbooruRetrieve"
+catalog = ""
+program_name = ki18n("Danbooru Client")
+version = "1.0"
+description = ki18n("A client for Danbooru sites.")
+license = KAboutData.License_GPL
+copyright = ki18n("(C) 2009 Luca Beltrame")
+text = ki18n("Test")
+home_page = "http://www.dennogumi.org"
+bug_email = "einar@heavensinferno.net"
 
-    URL = "http://www.konachan.com"
+about_data = KAboutData(app_name, catalog, program_name, version, description,
+                license, copyright, text, home_page, bug_email)
+KCmdLineArgs.init(sys.argv, about_data)
+app = KApplication()
+
+class TestDanbooruAPI(unittest.TestCase):
+
+    URL = "http://moe.imouto.org"
 
     def setUp(self):
-        self.api = api.Danbooru("http://www.konachan.net")
+
+        self.api = api.Danbooru(self.URL)
 
     def testData(self):
-        pass
 
-    def testGetPostList(self):
-        pass
+        "Post list retrieval"
+
+        data = self.api.get_post_list(limit=1)
+        self.assertTrue(data)
 
     def testGetThumbnailUrls(self):
-        pass
+
+        "Thumbnail list retrieval"
+
+        self.api.get_post_list(limit=1)
+        urls = self.api.get_thumbnail_urls()
+        self.assertEqual(len(urls), 1)
+        check = KIO.NetAccess.exists(urls[0], KIO.NetAccess.SourceSide, None)
+        self.assertTrue(check)
 
     def testGetPicture(self):
-        pass
 
+        "Image URL and image retrieval"
+
+        self.api.get_post_list(limit=1)
+        url = self.api.get_picture_url(0)
+        check = KIO.NetAccess.exists(url, KIO.NetAccess.SourceSide, None)
+        self.assertTrue(check)
+        picture = self.api.get_image(url, verbose=True)
+        self.assertFalse(picture.isNull())
+        
+def main():
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestDanbooruAPI)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+if __name__ == "__main__":
+    main()
