eec7f7e93bcf8041a552a5726d7c8ce55337d98e
Luca Beltrame <einar@heavensinferno.net>
2009-10-24 15:43:04 +0200

    Split up the configuration in three pages, and clean up imports

--- preferences.py
+++ preferences.py
@@ -23,15 +23,25 @@ Author: Luca Beltrame
 Description: Preferences module for the Danbooru client.
 '''
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
-from PyKDE4.kdeui import *
+from PyQt4.QtCore import QStringList, QSize, QRegExp, QRegExpValidator
+from PyQt4.QtGui import QWidget
+from PyKDE4.kdeui import KConfigSkeleton, KConfigDialog, KIcon, KDialog
+from PyKDE4.kdecore import i18n
 
 from ui_generalpage import Ui_GeneralPage
+from ui_nepomukpage import Ui_NepomukPage
+from ui_danboorupage import Ui_DanbooruPage
 
 class Preferences(KConfigSkeleton):
 
-    """Class to handle preferences."""
+    """Class to handle preferences. Currently, the following items are stored:
+        - danbooruUrls - list of Danbooru URLs inserted by the user
+        - thumbnailMaxRetrieve - maximum number of thumbnail to retireve
+        - nepomukEnabled - whether to use Nepomuk tagging or not
+        - tagBlacklist - tags that should not be used while tagging
+        - columnNumber - number of columns to display
+
+        Currently usernames and passwords are not saved at all."""
 
     def __init__(self, *args):
         KConfigSkeleton.__init__(self, *args)
@@ -59,7 +69,6 @@ class Preferences(KConfigSkeleton):
 
         self._column_number = self.addItemInt("columnNumber", 3, 3)
 
-        #TODO: Should user/passwords for the API be stored here?
         self.readConfig()
 
     @property
@@ -80,32 +89,86 @@ class Preferences(KConfigSkeleton):
 
     @property
     def tag_blacklist(self):
-        return self;_tag_blacklist.value()
+        return self._tag_blacklist.value()
 
 class PreferencesDialog(KConfigDialog):
 
     def __init__(self, parent=None, name=None, preferences=None):
-        super(PreferencesDialog, self).__init__(parent, name, preferences)
 
+        super(PreferencesDialog, self).__init__(parent, name, preferences)
         self.setButtons(KDialog.ButtonCode(KDialog.Ok |KDialog.Apply |
                                             KDialog.Cancel))
 
+        self.resize(QSize(550, 420))
         self.general_page = GeneralPage(self, preferences)
-        self.general_page_item = self.addPage(self.general_page, 'General')
-        self.general_page_item.setIcon(KIcon("preferences-web-browser-shortcuts"))
+        self.nepomuk_page = NepomukPage(self, preferences)
+        self.general_page_item = self.addPage(self.general_page,
+                                              i18n('Layout'))
+
+        self.danbooru_page = DanbooruPage(self, preferences)
+        self.danbooru_page_item = self.addPage(self.danbooru_page,
+                                               i18n("Danbooru URLs"))
+
+        self.nepomuk_page_item = self.addPage(self.nepomuk_page,
+                                              i18n("Tagging"))
+
+        self.general_page_item.setIcon(KIcon("table"))
+        self.danbooru_page_item.setIcon(KIcon("preferences-web-browser-shortcuts"))
+        self.nepomuk_page_item.setIcon(KIcon("nepomuk"))
+
 
 class GeneralPage(QWidget, Ui_GeneralPage):
+
+    "Page containing layout options"
 
     def __init__(self, parent=None, preferences=None):
 
         super(GeneralPage, self).__init__(parent)
         self.setupUi(self)
 
-        self.kcfg_danbooruUrls.insertStringList(preferences.boards_list)
         self.kcfg_thumbnailMaxRetrieve.setValue(preferences.thumbnail_no)
+        self.kcfg_displayColumns.setValue(preferences.column_no)
 
+
+class NepomukPage(QWidget, Ui_NepomukPage):
+
+    "Page containing Nepomuk options"
+
+    def __init__(self, parent=None, preferences=None):
+
+        super(NepomukPage, self).__init__(parent)
+        self.setupUi(self)
+
+        self.kcfg_nepomukEnabled.setChecked(preferences.nepomuk_enabled)
+        self.kcfg_tagBlacklist.insertStringList(preferences.tag_blacklist)
+
+        self.kcfg_nepomukEnabled.stateChanged.connect(self.toggle_editlist)
+
+        if self.kcfg_nepomukEnabled.isChecked():
+            self.kcfg_tagBlacklist.setDisabled(False)
+
+    def toggle_editlist(self, state):
+
+        if state == Qt.Checked:
+            self.kcfg_tagBlacklist.setDisabled(False)
+        elif state == Qt.Unchecked:
+            self.kcfg_tagBlacklist.setDisabled(True)
+
+
+class DanbooruPage(QWidget, Ui_DanbooruPage):
+
+    "Page containing Danbooru URLs"
+
+    def __init__(self, parent=None, preferences=None):
+
+        super(DanbooruPage, self).__init__(parent)
+        self.setupUi(self)
+
+        self.kcfg_danbooruUrls.insertStringList(preferences.boards_list)
         regex =(r"(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?")
         regex = QRegExp(regex)
 
         self._validator = QRegExpValidator(regex, self)
         self.kcfg_danbooruUrls.lineEdit().setValidator(self._validator)
+
+
