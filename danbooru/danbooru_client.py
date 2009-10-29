2bc688e85b6969738589625cb959d1b444b32b12
Luca Beltrame <einar@heavensinferno.net>
2009-10-29 22:47:05 +0100

    Bump version, add catalog

--- danbooru/danbooru_client.py
+++ danbooru/danbooru_client.py
@@ -32,9 +32,9 @@ import mainwindow
 def main():
 
     app_name="danbooru_client"
-    catalog = "danbooru_client.pot"
+    catalog = "danbooru_client"
     program_name = ki18n("Danbooru Client")
-    version = "0.1"
+    version = "0.2"
     description = ki18n("A client for Danbooru sites.")
     license = KAboutData.License_GPL
     copyright = ki18n("(C) 2009 Luca Beltrame")
