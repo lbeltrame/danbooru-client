8b300e06cfd3f76414946f54fe2c25ed60714471
Luca Beltrame <einar@heavensinferno.net>
2009-12-27 00:07:02 +0100

    Proper Messages.sh

--- Messsages.sh
+++ Messsages.sh
@@ -1,28 +1,27 @@
-#!/bin/bash - 
-#===============================================================================
-#
-#          FILE:  Messsages.sh
-# 
-#         USAGE:  ./Messsages.sh 
-# 
-#   DESCRIPTION:  
-# 
-#       OPTIONS:  ---
-#  REQUIREMENTS:  ---
-#          BUGS:  ---
-#         NOTES:  ---
-#        AUTHOR:   (), 
-#       COMPANY:  
-#       VERSION:  1.0
-#       CREATED:  24/10/2009 14:45:01 CEST
-#      REVISION:  ---
-#===============================================================================
+#!/bin/bash
 
-set -o nounset                              # Treat unset variables as an error
+EXTRACTRC="/usr/bin/extractrc"
+XGETTEXT="/usr/bin/xgettext --from-code=UTF-8 -C -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3"
+PODIR="/home/lb/Coding/pykde4/danbooru/po"
+PROJECT="danbooru_client"
 
-XGETTEXT="xgettext --from-code=UTF-8 -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 \
-          -ki18ncp:1c,2,3 -ktr2i18n:1 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale \
-          -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3xgettext -ki18n -ki18nc
-          -ki18ncp -ki18np"
+# invoke the extractrc script on all .ui, .rc, and .kcfg files in the sources
+# the results are stored in a pseudo .py file to be picked up by xgettext.
+echo "Extracting messages"
+$EXTRACTRC --language=Python `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.py
+# call xgettext on all source files. If your sources have other filename
+# extensions besides .py, just add them in the find call.
+$XGETTEXT -L python `find . -name \*.py` -o $PODIR/${PROJECT}.pot
 
-$XGETTEXT *.py -o danbooru_client.pot
+echo "Merging translations"
+catalogs=`find po/ -name '*.po'`
+for cat in $catalogs; do
+	echo $cat
+	msgmerge -o $cat.new $cat $PODIR/${PROJECT}.pot
+	mv $cat.new $cat
+done
+echo "Done merging translations"
+
+echo "Cleaning up"
+#rm rc.py
+echo "Done"
