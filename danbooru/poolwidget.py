755510e594a68f0b239b45c7cd5ea5b37b2e7847
Luca Beltrame <einar@heavensinferno.net>
2011-07-25 21:35:11 +0200

    Stub for a pool widget

---
+++ danbooru/poolwidget.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#   Copyright 2011 Luca Beltrame <einar@heavensinferno.net>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License, under
+#   version 2 of the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the
+#   Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import PyQt4.QtCore as QtCore
+import PyQt4.QtGui as QtGui
+
+import PyKDE4.kdecore as kdecore
+import PyKDE4.kdeui as kdeui
