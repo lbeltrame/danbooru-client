16fb8616c3d5fd27e38690404e0fe699390fe013
Luca Beltrame <einar@heavensinferno.net>
2011-09-25 20:34:49 +0200

    Fix build system and improve Messages.sh

--- Messsages.sh
+++ Messsages.sh
@@ -2,7 +2,7 @@
 
 EXTRACTRC="/usr/bin/extractrc"
 XGETTEXT="/usr/bin/xgettext --from-code=UTF-8 -C -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3"
-PODIR="/home/lb/Coding/pykde4/danbooru/po"
+PODIR="/home/lb/Coding/Python/pykde4/danbooru/po"
 PROJECT="danbooru_client"
 
 # invoke the extractrc script on all .ui, .rc, and .kcfg files in the sources
