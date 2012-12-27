ca51f545ba3ca5410c6b36272f2efcd546e11d66
Luca Beltrame <lbeltrame@kde.org>
2012-12-27 20:55:31 +0100

    Support KWallet for storing passwords, close issue #5.
    This requires a mapping for password salts, which is kept statically for now. If a salt is missing,
    the password is not used.

--- danbooru/connectwidget.py
+++ danbooru/connectwidget.py
@@ -17,6 +17,8 @@
 #   Free Software Foundation, Inc.,
 #   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import hashlib
+
 import PyQt4.QtCore as QtCore
 import PyQt4.QtGui as QtGui
 
@@ -26,6 +28,14 @@ import PyKDE4.kdeui as kdeui
 import api.remote as remote
 from ui.ui_connectwidget import Ui_connectForm
 
+_SALTS = {"http://yande.re": "choujin-steiner--{}--",
+          "http://konachan.com": "So-I-Heard-You-Like-Mupkids-?--{}--",
+          "http://konachan.net": "So-I-Heard-You-Like-Mupkids-?--{}--",
+          "http://danbooru.donmai.us": "choujin-steiner--{}--"}
+
+
+Wallet = kdeui.KWallet.Wallet
+KWallet = kdeui.KWallet
 
 class ConnectWidget(QtGui.QWidget, Ui_connectForm):
 
@@ -45,10 +55,21 @@ class ConnectWidget(QtGui.QWidget, Ui_connectForm):
 
         self._connection = None
 
+        winid = self.parent().winId()
+
         self.setup_urls(urls)
 
         self.buttonBox.accepted.connect(self.accept)
         self.closeButton.clicked.connect(self.rejected.emit)
+
+        self._wallet_available = False
+        self._wallet = Wallet.openWallet(Wallet.NetworkWallet(), winid,
+                                         Wallet.Asynchronous)
+        self._wallet.walletOpened.connect(self._check_wallet)
+
+        self.danbooruUrlComboBox.currentIndexChanged[QtCore.QString].connect(
+            self._adjust_wallet)
+
 
     @property
     def url(self):
@@ -57,19 +78,20 @@ class ConnectWidget(QtGui.QWidget, Ui_connectForm):
 
         return self.danbooruUrlComboBox.currentText()
 
-    @property
-    def username(self):
+    def _set_username(self, username):
+        self.userLineEdit.setText(username)
 
-        "Returns the inserted username."
+    def _get_username(self):
+            return self.userLineEdit.text()
 
-        return self.userLineEdit.text()
+    def _set_password(self, password):
+        self.passwdLineEdit.setText(password)
 
-    @property
-    def password(self):
-
-        "Returns the inserted password."
-
+    def _get_password(self):
         return self.passwdLineEdit.text()
+
+    username = property(_get_username, _set_username)
+    password = property(_get_password, _set_password)
 
     def accept(self):
 
@@ -77,11 +99,26 @@ class ConnectWidget(QtGui.QWidget, Ui_connectForm):
         login = self.username
         password = self.password
 
+        if url.isEmpty():
+            return
+
         username = None if login.isEmpty() else login
         password = None if password.isEmpty() else password
 
-        if url.isEmpty():
-            return
+        salt = _SALTS.get(unicode(url))
+
+        if salt is None:
+            username = None
+            password = None
+
+        if self._wallet_available:
+            if not self._wallet.hasEntry(self.url):
+                data_map = dict(username=self.username, password=self.password)
+                self._wallet.writeMap(self.url, data_map)
+
+        if username is not None and password is not None:
+            password = salt.format(password)
+            password = hashlib.sha1(password).hexdigest()
 
         self._connection = remote.DanbooruService(unicode(url), username,
                                                   password=password)
@@ -97,7 +134,34 @@ class ConnectWidget(QtGui.QWidget, Ui_connectForm):
                 self.danbooruUrlComboBox.insertUrl(index,
                     kdecore.KUrl(item))
 
+    def _adjust_wallet(self, name):
 
+        self.username = QtCore.QString()
+        self.password = QtCore.QString()
+
+        if not self._wallet_available:
+            return
+
+        if self._wallet.hasEntry(name):
+            code, data_map = self._wallet.readMap(name)
+
+            if code != 0:
+                return
+
+            self.username = data_map[QtCore.QString("username")]
+            self.password = data_map[QtCore.QString("password")]
+
+    def _check_wallet(self, result):
+
+        if not result:
+            return
+
+        self._wallet_available = True
+
+        if not self._wallet.hasFolder(Wallet.PasswordFolder()):
+           self._wallet.createFolder(Wallet.PasswordFolder())
+
+        self._adjust_wallet(self.danbooruUrlComboBox.currentText())
 
 
 
